cf8d33abacfb9465852856b0a32ea4e8e89dbbee
Aditya <adityaanandx@gmail.com>
2025-06-20 21:13:36 +0530

    x

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ const Header = () => {
         <div className="flex justify-between items-center py-4">
           <div className="text-2xl font-bold">ECOMMERCE</div>
           <nav className="hidden md:flex space-x-8">
-            <a href="#" className="text-gray-600 hover:text-black">Categories</a>
-            <a href="#" className="text-gray-600 hover:text-black">Sale</a>
-            <a href="#" className="text-gray-600 hover:text-black">Clearance</a>
-            <a href="#" className="text-gray-600 hover:text-black">New stock</a>
-            <a href="#" className="text-gray-600 hover:text-black">Trending</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Categories</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Sale</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Clearance</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">New stock</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Trending</a>
           </nav>
           <div className="flex items-center space-x-4">
-            <a href="#" className="text-gray-600 hover:text-black">Help</a>
-            <a href="#" className="text-gray-600 hover:text-black">Orders & Returns</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Help</a>
+            <a href="/signup" className="text-gray-600 hover:text-black">Orders & Returns</a>
             <span>Hi, John</span>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
